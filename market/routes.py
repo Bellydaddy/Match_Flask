189d94a41f24312a300cd0f6ae524b6ccfb5a86d
Bellydaddy <40156848+Bellydaddy@users.noreply.github.com>
2022-01-03 08:45:02 -0600

    Initial commit

---
+++ market/routes.py
@@ -0,0 +1,51 @@
+from market import app
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from market.models import Item, User
+from market.forms import RegisterForm, LoginForm
+from market import db
+
+# main page
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+# about page
+@app.route('/about/<username>')
+def about_page(username):
+    return '<h1>This is the about page of {}</h1>'.format(username)
+
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, email_address=form.email_address.data, password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash('There was an error with creating a user: {}'.format(err_msg), category='danger')
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+
+
+
+
+
+
+
